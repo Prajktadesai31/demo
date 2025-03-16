ae1a692fec0083863b5342abbc40df90606dbddc
Prajktadesai31 <126471578+Prajktadesai31@users.noreply.github.com>
2025-03-16 15:52:28 +0530

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,24 @@
+
+//Demo on hello
+#include<stdio.h>   //mandatory section
+
+int main()       //mandatory section
+{
+    
+    int a; //declaration
+    a=7 ;  //initialization
+
+    int b=3; 
+    printf("a"); //a
+    printf("\n %d" , a);      //7
+    printf("\n res=%d",(a+b));   //10
+    printf("\n res=%d",(a-b));   //4
+    printf("\n res=%d",(a*b));   //21
+    printf("\n res=%d",(a/b));   //2
+    printf("\n res=%d",(a%b));   //1
+
+    
+  return 0;
+}
+
+
