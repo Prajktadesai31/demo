8a66f392afc059939b692ebb983f060131eb91cd
Prajkta31 <vedashree318@gmail.com>
2025-06-22 10:53:06 +0530

    file added

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+//using namespace std;
+int main()
+{
+std:: cout<< "hello";    //cout--object--to represent standard o/p stream
+   return 0;             //stream--sequence of character
+}                        // << --stream insertion operator
+
+/*
+
+
+C:\Users\Admin\Desktop>g++ hello.cpp -o hello
+
+C:\Users\Admin\Desktop>hello
+hello
+
+
+*/
